79f3f947daf2e39561aaa1ea8eabb7e6abf8c2cc
danijanos <dani.janos@gmail.com>
2017-12-09 19:35:19 +0100

    Uncomment ViewModelLocator's construcror's if statement

--- OE-NIK-ImPro/UI/UI.Desktop/ViewModel/ViewModelLocator.cs
+++ OE-NIK-ImPro/UI/UI.Desktop/ViewModel/ViewModelLocator.cs
@@ -31,16 +31,14 @@ namespace OE.NIK.ImPro.UI.Desktop.ViewModel
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
-            ////if (ViewModelBase.IsInDesignModeStatic)
-            ////{
-            ////    // Create design time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
-            ////}
-            ////else
-            ////{
-            ////    // Create run time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DataService>();
-            ////}
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                // Create design time view services and models                
+            }
+            else
+            {
+                // Create run time view services and models                
+            }
 
             SimpleIoc.Default.Register<MainViewModel>();
         }
