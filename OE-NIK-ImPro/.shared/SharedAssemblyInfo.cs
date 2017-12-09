b2d68a243eb110d4ec06a426028c996c9edcc59d
danijanos <dani.janos@gmail.com>
2017-12-09 15:12:24 +0100

    Add SharedAssemblyInfo.cs to .shared folder

---
+++ OE-NIK-ImPro/.shared/SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("danijanos")]
+[assembly: AssemblyProduct("OE-NIK-ImPro")]
+[assembly: AssemblyCopyright("danijanos 2017")]
+[assembly: AssemblyTrademark("danijanos")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
