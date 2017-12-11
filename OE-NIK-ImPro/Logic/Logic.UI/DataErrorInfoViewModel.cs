47c83c39e439b40a0bf2bfe6e43b0bdd9f96e370
danijanos <dani.janos@gmail.com>
2017-12-11 12:30:11 +0100

    Add DataErrorInfoViewModel class to Logic.UI project

---
+++ OE-NIK-ImPro/Logic/Logic.UI/DataErrorInfoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace OE.NIK.ImPro.Logic.UI
+{
+    /// <summary>
+    /// ViewModel for IDataErrorInfo
+    /// </summary>
+    class DataErrorInfoViewModel : ViewModelBase
+    {
+
+    }
+}
