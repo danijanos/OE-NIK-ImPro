215ca7857669e7f1ac3304383fa4ade349143108
danijanos <dani.janos@gmail.com>
2017-12-12 16:51:02 +0100

    Add private int[][] _rgbColor filed which stores [Luminosity, Red, Green, Blue] to HistogramCalculator

--- OE-NIK-ImPro/Logic/Logic.UI/Models/HistogramCalculator.cs
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/HistogramCalculator.cs
@@ -2,11 +2,16 @@
 
 namespace OE.NIK.ImPro.Logic.UI.Models
 {
-    class HistogramCalculator
+    public class HistogramCalculator
     {
+        /// <summary>
+        /// filed which stores [Luminosity, Red, Green, Blue]
+        /// </summary>
+        private int[][] _rgbColor;
+
         public HistogramCalculator(Bitmap sourceImage)
         {
-            
+            _rgbColor = new[] { new int[256], new int[256], new int[256], new int[256] };
         }
     }
 }
