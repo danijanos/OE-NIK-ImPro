bca2faa70860868e8ba6542fe3a0ba4b443f141f
danijanos <dani.janos@gmail.com>
2017-12-12 12:52:27 +0100

    Implement InitMessenger method in MessageListener and add new HistogramWindow to UI.Desktop project

--- OE-NIK-ImPro/UI/UI.Desktop/MessageListener.cs
+++ OE-NIK-ImPro/UI/UI.Desktop/MessageListener.cs
@@ -1,15 +1,30 @@
-﻿namespace OE.NIK.ImPro.UI.Desktop
+﻿using GalaSoft.MvvmLight.Messaging;
+using OE.NIK.ImPro.Logic.UI;
+
+namespace OE.NIK.ImPro.UI.Desktop
 {
     public class MessageListener
     {
+        /// <summary>
+        /// Initialize a new instance of <see cref="MessageListener"/> class
+        /// </summary>
         public MessageListener()
         {
             InitMessenger();
         }
 
+        /// <summary>
+        /// Called by the constructor to define the message we are interested in
+        /// </summary>
         private void InitMessenger()
         {
-            throw new System.NotImplementedException();
+            Messenger.Default.Register<HistogramPresenter>(
+                this,
+                msg =>
+                {
+                    var window = new HistogramWindow();
+                }
+                );
         }
     }
 }
