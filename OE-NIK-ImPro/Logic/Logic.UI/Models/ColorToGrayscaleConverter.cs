f1d3c7827383269480699243e0afe2281757fab6
danijanos <dani.janos@gmail.com>
2017-12-13 12:23:33 +0100

    Inherit ColorToGrayscaleConverter class from BaseImageProcesser and change it's constructor

--- OE-NIK-ImPro/Logic/Logic.UI/Models/ColorToGrayscaleConverter.cs
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/ColorToGrayscaleConverter.cs
@@ -1,10 +1,12 @@
-﻿namespace OE.NIK.ImPro.Logic.UI.Models
-{
-    public class ColorToGrayscaleConverter
-    {
-        public ColorToGrayscaleConverter(string filePath)
-        {
+﻿using System.Drawing;
 
+namespace OE.NIK.ImPro.Logic.UI.Models
+{
+    public class ColorToGrayscaleConverter : BaseImageProcesser
+    {
+        public ColorToGrayscaleConverter(Bitmap sourceImage) : base(sourceImage)
+        {
+            
         }
     }
 }
