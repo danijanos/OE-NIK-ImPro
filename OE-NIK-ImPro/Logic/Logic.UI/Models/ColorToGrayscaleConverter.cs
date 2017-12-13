8ee29c71b6e20b6b6454fdfdfaa9611fc00205e8
danijanos <dani.janos@gmail.com>
2017-12-13 12:39:38 +0100

    Make ColorToGrayscaleConverter class sealed and add documentation

--- OE-NIK-ImPro/Logic/Logic.UI/Models/ColorToGrayscaleConverter.cs
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/ColorToGrayscaleConverter.cs
@@ -2,8 +2,16 @@
 
 namespace OE.NIK.ImPro.Logic.UI.Models
 {
-    public class ColorToGrayscaleConverter : BaseImageProcesser
+    /// <inheritdoc />
+    /// <summary>
+    /// Class which converts color images to grayscale
+    /// </summary>
+    public sealed class ColorToGrayscaleConverter : BaseImageProcesser
     {
+        /// <summary>
+        /// Initilaize a new instance of the <see cref="ColorToGrayscaleConverter"/> class
+        /// </summary>
+        /// <param name="sourceImage">The source of the image</param>
         public ColorToGrayscaleConverter(Bitmap sourceImage) : base(sourceImage)
         {
             
