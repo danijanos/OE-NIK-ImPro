b67df2b93d0f8b27f17ec85ee750a6518e4ea087
danijanos <dani.janos@gmail.com>
2017-12-13 11:40:35 +0100

    Add summary and and base read only properties to BaseImageProcesser

--- OE-NIK-ImPro/Logic/Logic.UI/Models/BaseImageProcesser.cs
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/BaseImageProcesser.cs
@@ -2,11 +2,28 @@
 
 namespace OE.NIK.ImPro.Logic.UI.Models
 {
+    /// <summary>
+    /// Base for image processer classes
+    /// </summary>
     public abstract class BaseImageProcesser
     {
+        /// <summary>
+        /// Initilaize a new instance of a <see cref="BaseImageProcesser"/> class
+        /// </summary>
+        /// <param name="sourceImage">The source of the image</param>
         protected BaseImageProcesser(Bitmap sourceImage)
         {
             
         }
+
+        /// <summary>
+        /// Gets the width of the source image
+        /// </summary>
+        public int ImageWidth { get; private set; }
+
+        /// <summary>
+        /// Gets the heigth of the source image
+        /// </summary>
+        public int ImageHeight { get; private set; }
     }
 }
