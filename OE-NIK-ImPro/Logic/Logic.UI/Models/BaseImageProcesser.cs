6ffa6eb403c32bb9b8caf3e5dba144b677812270
danijanos <dani.janos@gmail.com>
2017-12-13 11:57:01 +0100

    Finalizing BaseImageProcesser class. (add IsImageGrayscale bool property,SourceImageToBmData BitmapData property , LockSourceImageBits method and give documentation to all of it)

--- OE-NIK-ImPro/Logic/Logic.UI/Models/BaseImageProcesser.cs
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/BaseImageProcesser.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace OE.NIK.ImPro.Logic.UI.Models
 {
@@ -15,6 +16,12 @@ namespace OE.NIK.ImPro.Logic.UI.Models
         {
             ImageWidth = sourceImage.Width;
             ImageHeight = sourceImage.Height;
+            IsImageGrayscale = (sourceImage.PixelFormat == PixelFormat.Format8bppIndexed);
+
+            // lock bitmap data
+            LockSourceImageBits(sourceImage);
+            // Todo: call the function that consumes and process the image
+            sourceImage.UnlockBits(SourceImageToBmData);
         }
 
         /// <summary>
@@ -26,5 +33,30 @@ namespace OE.NIK.ImPro.Logic.UI.Models
         /// Gets the heigth of the source image
         /// </summary>
         public int ImageHeight { get; private set; }
+
+        /// <summary>
+        /// Determines that the source image is weather grayscale or not
+        /// </summary>
+        public bool IsImageGrayscale { get; private set; }
+
+        /// <summary>
+        /// Stores the source image as BitmapData
+        /// </summary>
+        public BitmapData SourceImageToBmData { get; private set; }
+
+        /// <summary>
+        /// Locks the bits of the given bitmap image and add it to <see cref="SourceImageToBmData"/> property for further processing
+        /// </summary>
+        /// <param name="sourceImage">The source of the image as a Bitmap</param>
+        private void LockSourceImageBits(Bitmap sourceImage)
+        {
+            SourceImageToBmData = sourceImage.LockBits(
+                new Rectangle(0, 0, ImageWidth, ImageHeight),
+                ImageLockMode.ReadOnly,
+                (IsImageGrayscale ? PixelFormat.Format8bppIndexed : PixelFormat.Format24bppRgb)
+            );
+        }
+
+        // TODO: perhaps need to create a ImageLockMode.ReadWrite version?
     }
 }
