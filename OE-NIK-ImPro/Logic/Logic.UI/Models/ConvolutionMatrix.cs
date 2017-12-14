d285a02f971412771ae4fdf4e62bdad646b8ac75
danijanos <dani.janos@gmail.com>
2017-12-14 12:53:38 +0100

    Implement basic properties for ConvolutionMatrix

--- OE-NIK-ImPro/Logic/Logic.UI/Models/ConvolutionMatrix.cs
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/ConvolutionMatrix.cs
@@ -5,6 +5,17 @@
     /// </summary>
     public class ConvolutionMatrix
     {
+        public int TopLeft = 0, TopMid = 0, TopRight = 0;
+        public int MidLeft = 0, Pixel = 1, MidRight = 0;
+        public int BottomLeft = 0, BottomMid = 0, BottomRight = 0;
 
+        public int Factor = 1;
+        public int Offset = 0;
+
+        public void SetWithValue(int value)
+        {
+            TopLeft = TopMid = TopRight = MidLeft = Pixel = MidRight =
+                BottomLeft = BottomMid = BottomRight = value;
+        }
     }
 }
