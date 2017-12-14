a9371b5db657e98c2676f262b6659e414cbe1df2
danijanos <dani.janos@gmail.com>
2017-12-14 13:12:18 +0100

    Add Convolution class as a BaseImageProcesser child

---
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/Convolution.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace OE.NIK.ImPro.Logic.UI.Models
+{
+    internal class Convolution : BaseImageProcesser
+    {
+        public Convolution(Bitmap sourceImage) : base(sourceImage)
+        {
+
+        }
+    }
+}
