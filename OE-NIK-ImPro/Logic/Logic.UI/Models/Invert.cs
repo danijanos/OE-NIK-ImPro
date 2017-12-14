165018b7e72b20ff8e8c6cb907f1d5eca3d055d8
danijanos <dani.janos@gmail.com>
2017-12-14 11:01:27 +0100

    Add new Invert class to Logic.UI project's Models

---
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/Invert.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace OE.NIK.ImPro.Logic.UI.Models
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Class which inverts color images
+    /// </summary>
+    internal sealed class Invert : BaseImageProcesser
+    {
+        /// <summary>
+        /// Initilaize a new instance of the <see cref="Invert"/> class
+        /// </summary>
+        /// <param name="sourceImage">The source of the image</param>
+        public Invert(Bitmap sourceImage) : base(sourceImage)
+        {
+
+        }
+    }
+}
