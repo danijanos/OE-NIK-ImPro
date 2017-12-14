883e99e47f90df98f8cfac36e76fff216ac67f1f
danijanos <dani.janos@gmail.com>
2017-12-14 12:47:20 +0100

    Add new ConvolutionMatrix to Logic.UI project

---
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/ConvolutionMatrix.cs
@@ -0,0 +1,10 @@
+﻿namespace OE.NIK.ImPro.Logic.UI.Models
+{
+    /// <summary>
+    /// Class which describe a convoluton matrix for image processing
+    /// </summary>
+    public class ConvolutionMatrix
+    {
+
+    }
+}
