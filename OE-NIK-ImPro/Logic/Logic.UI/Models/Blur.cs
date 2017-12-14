8d151a3365cce0171299d798bcc53a784ac78f12
danijanos <dani.janos@gmail.com>
2017-12-14 13:06:59 +0100

    Add Blur class to Logic.UI project

---
+++ OE-NIK-ImPro/Logic/Logic.UI/Models/Blur.cs
@@ -0,0 +1,10 @@
+﻿namespace OE.NIK.ImPro.Logic.UI.Models
+{
+    /// <summary>
+    /// Class which applies blurring operation in the image
+    /// </summary>
+    internal sealed class Blur
+    {
+
+    }
+}
